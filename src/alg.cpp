c491b20f770bc056651747165feab377b5fe2968
Малышева Галина <99182939+Malychwa@users.noreply.github.com>
2022-02-13 18:31:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,13 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    while ((a != 0) && (b != 0)){
-        if(a>=b){
-            a = a % b;
-        }
-        else{
-            b = b % a;
-        }
+while ((a != 0) && (b != 0)){
+    if(a>=b){
+        a = a % b;
     }
-    return (a+b);
+    else{
+        b = b % a;
+    }
+}
+return (a+b);
 }
